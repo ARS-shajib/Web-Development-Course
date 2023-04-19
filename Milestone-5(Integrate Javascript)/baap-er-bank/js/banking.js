ba6c4ef7ae4fdf9c79ede6ddb144d3c4279555e8
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-04-20 04:39:01 +0600

    25.5 done

---
+++ Milestone-5(Integrate Javascript)/baap-er-bank/js/banking.js
@@ -0,0 +1,6 @@
+document.getElementById('deposit-button').addEventListener('click', function () {
+    // get the amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+})
