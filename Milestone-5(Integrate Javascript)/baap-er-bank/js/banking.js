850d3660e139b674a30307cf53dc21e9677e1d4d
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-04-20 08:50:56 +0600

    module 26 done

--- Milestone-5(Integrate Javascript)/baap-er-bank/js/banking.js
+++ Milestone-5(Integrate Javascript)/baap-er-bank/js/banking.js
@@ -2,21 +2,27 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // get the amount deposited
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
+    if (parseFloat(depositAmount) > 0) {
+        const depositTotal = document.getElementById('deposit-total');
+        depositTotal.innerText = parseFloat(depositAmount) + parseFloat(depositTotal.innerText);
 
-    const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = parseFloat(depositAmount) + parseFloat(depositTotal.innerText);
+        const balance = document.getElementById('balance-total');
+        balance.innerText = parseFloat(balance.innerText) + parseFloat(depositAmount);
+    }
     depositInput.value = '';
-    const balance = document.getElementById('balance-total');
-    balance.innerText = parseFloat(balance.innerText) + parseFloat(depositAmount);
 })
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const withdrawInput = document.getElementById('withdraw-input');
     const withdrawAmount = withdrawInput.value;
 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawAmount);
-    withdrawInput.value = '';
     const balance = document.getElementById('balance-total');
-    balance.innerText = parseFloat(balance.innerText) - parseFloat(withdrawAmount);
+
+    if (parseFloat(withdrawAmount) > 0 && parseFloat(withdrawAmount) <= parseFloat(balance.innerText)) {
+        const withdrawTotal = document.getElementById('withdraw-total');
+        withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawAmount);
+
+        balance.innerText = parseFloat(balance.innerText) - parseFloat(withdrawAmount);
+    }
+    withdrawInput.value = '';
 })
