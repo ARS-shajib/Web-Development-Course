71994094694607d22dcc7f5131cf285ce865eda8
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-04-20 08:04:31 +0600

    26.1 done

--- Milestone-5(Integrate Javascript)/baap-er-bank/js/banking.js
+++ Milestone-5(Integrate Javascript)/baap-er-bank/js/banking.js
@@ -2,5 +2,21 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // get the amount deposited
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
-    console.log(depositAmount);
+
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = parseFloat(depositAmount) + parseFloat(depositTotal.innerText);
+    depositInput.value = '';
+    const balance = document.getElementById('balance-total');
+    balance.innerText = parseFloat(balance.innerText) + parseFloat(depositAmount);
+})
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    withdrawTotal.innerText = parseFloat(withdrawTotal.innerText) + parseFloat(withdrawAmount);
+    withdrawInput.value = '';
+    const balance = document.getElementById('balance-total');
+    balance.innerText = parseFloat(balance.innerText) - parseFloat(withdrawAmount);
 })
