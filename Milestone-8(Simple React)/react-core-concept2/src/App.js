d4cdbd5211e4de22761e42fc169996b4d2dfffaf
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-05-09 08:29:21 +0600

    module 45 done

--- Milestone-8(Simple React)/react-core-concept2/src/App.js
+++ Milestone-8(Simple React)/react-core-concept2/src/App.js
@@ -1,20 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
-function Counter() {
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
 
   return (
     <div>
-      <h1>count: 0</h1>
-      <button>Increase</button>
+      <h3>External Users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  const css = {
+    backgroundColor: "lightblue",
+    border: "2px solid lightsalmon",
+  }
+  return (
+    <div style={css}>
+      <h2>name: {props.name}</h2>
+      <h2>email: {props.email}</h2>
+    </div>
+  )
+}
+
+
+
+function Counter() {
+  const [count, setCount] = useState(87);
+  const handleIncrease = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  };
+  return (
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   )
 }
