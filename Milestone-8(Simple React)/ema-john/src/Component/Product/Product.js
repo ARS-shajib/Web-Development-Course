1b942b24cff6e922b9c708ed46ee1e45482dd911
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-05-13 00:41:06 +0600

    Milestone 8 done

--- Milestone-8(Simple React)/ema-john/src/Component/Product/Product.js
+++ Milestone-8(Simple React)/ema-john/src/Component/Product/Product.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 const Product = (props) => {
-
-    const { name, img, seller, price, stock } = props.product;
+    // console.log('product -', props.product);
+    const { name, img, seller, price, stock, star } = props.product;
     const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className='product'>
@@ -16,6 +17,12 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: ${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    initialRating={star}
+                    emptySymbol="fa-regular fa-star icon-color"
+                    fullSymbol="fa-solid fa-star icon-color"
+                ></Rating>
+                <br />
                 <button
                     onClick={() => props.handleAddToCart(props.product)}
                     className='btn-regular'> {element} add to cart</button>
