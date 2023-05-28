e046ecb043fcbd7c82dc58a4be611fc3a82609f7
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-05-29 01:08:10 +0600

    63.3 done

--- Milestone-10(React Authentication)/responsive-react-media-query/src/App.js
+++ Milestone-10(React Authentication)/responsive-react-media-query/src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Booking from './Pages/Booking/Booking';
+import Login from './Pages/Login/Login/Login';
+import Header from './Pages/Shared/Header/Header';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
+    <div>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/booking/:serviceId' element={<Booking />} />
+            <Route path='*' element={<NotFound />} />
+
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
