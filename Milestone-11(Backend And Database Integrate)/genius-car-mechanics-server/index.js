e7009cf3630e9a1a7d86e896abe0308b1bfba29f
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-06-05 02:07:49 +0600

    module 65 done

--- Milestone-11(Backend And Database Integrate)/genius-car-mechanics-server/index.js
+++ Milestone-11(Backend And Database Integrate)/genius-car-mechanics-server/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const port = 5000;
 
-// user: genius-car-mechanics
-// pass: MJbEgZJuV6aEjBta
+// middleware
+app.use(cors());
+app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.utsh8g8.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.utsh8g8.mongodb.net/?retryWrites=true&w=majority";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,14 +28,50 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
+        const database = client.db('carMechanic');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.send(result);
+        });
+
+        //DELETE API
+        app.delete(`/services/:id`, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
