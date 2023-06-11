0e893a6c8076905998bb266ba26867da9a3f062f
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-06-11 19:23:40 +0600

    module 69 done

--- Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -1,10 +1,12 @@
-import { Grid, Typography } from '@mui/material';
+import { Box, Button, Grid, Typography } from '@mui/material';
 import React from 'react';
 import doctor from '../../../images/doctor.png';
 import bg from '../../../images/appointment-bg.png';
 
 const appointmentBanner = {
     background: `url(${bg})`,
+    backgroundColor: 'rgba(45, 58, 74, 0.9)',
+    backgroundBlendMode: 'darken, luminosity',
     marginTop: 175
 }
 
@@ -16,10 +18,26 @@ const AppointmentBanner = () => {
                     style={{ width: 400, marginTop: '-100px' }}
                     src={doctor} alt="" />
             </Grid>
-            <Grid xs={12} md={6}>
-                <Typography variant='h6'>
-                    Appointment
-                </Typography>
+            <Grid xs={12} md={6} sx={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                textAlign: 'left'
+            }}>
+                <Box>
+                    <Typography variant='h6' sx={{ mb: 5 }} style={{ color: '#22A4B6' }}>
+                        Appointment
+                    </Typography>
+                    <Typography variant='h4' style={{ color: 'white' }}>
+                        Make an Appointment Today
+                    </Typography>
+                    <Typography variant='h6' sx={{ my: 5 }} style={{
+                        color: 'white', fontSize: 14, fontWeight: 300
+                    }}>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae a quo doloremque quaerat. Cupiditate quas laudantium molestiae minima tempora molestias.
+                    </Typography>
+                    <Button variant='contained' style={{ backgroundColor: '#22A4B6' }}>Learn More</Button>
+                </Box>
             </Grid>
         </Grid>
     );
