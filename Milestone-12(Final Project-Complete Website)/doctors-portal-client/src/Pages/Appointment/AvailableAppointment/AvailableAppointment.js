9db6dd5ca89b479931352b7196a98b40f78f0733
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-06-27 13:02:45 +0600

    70.5 done

--- Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,96 @@
+import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
+import Booking from '../Booking/Booking';
+
+const bookings = [
+    {
+        id: 1,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 2,
+        name: 'Teeth Cleaning',
+        time: '10.00 AM - 11.00 AM',
+        space: 8,
+    },
+    {
+        id: 3,
+        name: 'Cosmetic Dentistry',
+        time: '09.00 AM - 10.00 AM',
+        space: 9,
+    },
+    {
+        id: 4,
+        name: 'Cavity Protection',
+        time: '07.00 AM - 08.00 AM',
+        space: 15,
+    },
+    {
+        id: 5,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 6,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 7,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 8,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 9,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 10,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 11,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    },
+    {
+        id: 12,
+        name: 'Teeth Orthodonics',
+        time: '08.00 AM - 09.00 AM',
+        space: 10,
+    }
+]
 
 const AvailableAppointment = ({ date }) => {
     return (
-        <div>
-            <h2>Available Appointment on {date.toString()}</h2>
-        </div>
+        <Container>
+            <Typography sx={{ color: 'info.main', my: 2 }} variant='h5'>Available Appointment on {date.toString()}</Typography>
+            <Grid container spacing={2}>
+                {
+                    bookings.map(booking => <Booking
+                        key={booking.id}
+                        booking={booking}
+                    >
+                    </Booking>)
+                }
+            </Grid>
+        </Container>
     );
 };
 
