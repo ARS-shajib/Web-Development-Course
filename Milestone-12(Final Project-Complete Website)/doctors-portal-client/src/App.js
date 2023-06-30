56e95a30751f4dbdebc9292e7dcc006d259c9481
ARS-shajib <abdurrahmanshajib253@gmail.com>
2023-06-30 17:17:27 +0600

    71.3 done

--- Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/App.js
+++ Milestone-12(Final Project-Complete Website)/doctors-portal-client/src/App.js
@@ -2,19 +2,27 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
 import './App.css';
 import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home></Home>} />
+          <Route path='/home' element={<Home></Home>} />
         </Routes>
         <Routes>
-          <Route path='/Home' element={<Home></Home>} />
+          <Route exact path='/' element={<Home></Home>} />
         </Routes>
         <Routes>
           <Route path='/appointment' element={<Appointment></Appointment>} />
+        </Routes>
+        <Routes>
+          <Route path='/login' element={<Login></Login>} />
+        </Routes>
+        <Routes>
+          <Route path='/register' element={<Register></Register>} />
         </Routes>
       </BrowserRouter>
     </div>
